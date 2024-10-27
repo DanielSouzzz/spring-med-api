d522bc382676c3499139cf47539282965ed74b96
daniel <danielsouzanovo@gmail.com>
2024-10-27 19:05:32 -0300

    Inclui o id no retorno do DTO de listagem do medico.

--- src/main/java/app/springmedapi/entity/dto/ListagemMedicoDTO.java
+++ src/main/java/app/springmedapi/entity/dto/ListagemMedicoDTO.java
@@ -2,5 +2,5 @@ package app.springmedapi.entity.dto;
 
 import app.springmedapi.enums.Especialidade;
 
-public record ListagemMedicoDTO(String nome, String email, String crm, Especialidade especialidade) {
+public record ListagemMedicoDTO(Long id, String nome, String email, String crm, Especialidade especialidade) {
 }
