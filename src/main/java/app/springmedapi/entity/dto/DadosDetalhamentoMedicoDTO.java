ab3b01fbaaa650d9b9660ffed7cfda0f12fa3352
daniel <danielsouzanovo@gmail.com>
2024-11-21 22:00:34 -0300

    Limpando arquivos.

--- src/main/java/app/springmedapi/entity/dto/DadosDetalhamentoMedicoDTO.java
+++ src/main/java/app/springmedapi/entity/dto/DadosDetalhamentoMedicoDTO.java
@@ -1,7 +1,5 @@
 package app.springmedapi.entity.dto;
 
-import app.springmedapi.entity.Medico;
-
 public record DadosDetalhamentoMedicoDTO(Long id, String nome, String email, String crm, String telefone,
                                          String especialidade, EnderecoDTO endereco) {
 }
