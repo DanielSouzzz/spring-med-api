1d13f02ada314803ec212255c92cfe092ab4772e
daniel <danielsouzanovo@gmail.com>
2024-11-27 00:11:02 -0300

    melhorando o retorno NotFound.

--- src/main/java/app/springmedapi/infra/GlobalExceptionHandler.java
+++ src/main/java/app/springmedapi/infra/GlobalExceptionHandler.java
@@ -1,14 +1,27 @@
 package app.springmedapi.infra;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity handleNotFoundException() {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<Map<String, Object>> handleNotFoundException(EntityNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+        response.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 }
