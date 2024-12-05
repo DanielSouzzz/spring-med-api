b21c7834bd492d3f158004c62b27e68ba33e687f
danielsouzzz <danielsouzanovo@gmail.com>
2024-12-04 22:00:02 -0300

    ajustes

--- src/main/java/app/springmedapi/service/GeolocationService.java
+++ src/main/java/app/springmedapi/service/GeolocationService.java
@@ -1,8 +1,18 @@
 package app.springmedapi.service;
 
+import app.springmedapi.service.external.OpenCageClient;
+import app.springmedapi.service.external.dto.GeolocationResultDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeolocationService {
+    private final OpenCageClient openCageClient;
 
+    public GeolocationService(OpenCageClient openCageClient) {
+        this.openCageClient = openCageClient;
+    }
+
+    public GeolocationResultDTO fetchCoordinates(String address) {
+        return openCageClient.fetchCoordinates(address);
+    }
 }
