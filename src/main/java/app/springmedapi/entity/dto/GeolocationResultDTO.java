15300479edd581d9ff927216da05c1b78e2b9228
danielsouzzz <danielsouzanovo@gmail.com>
2024-12-04 22:00:58 -0300

    conlui o desenvolvimento da requisicao para obter a let e long do endereco.

--- src/main/java/app/springmedapi/entity/dto/GeolocationResultDTO.java
+++
@@ -1,4 +0,0 @@
-package app.springmedapi.entity.dto;
-
-public record GeolocationResultDTO(double latitude, double longitude) {
-}
---
+++ src/main/java/app/springmedapi/service/external/dto/GeolocationRequestDTO.java
@@ -0,0 +1,4 @@
+package app.springmedapi.service.external.dto;
+
+public record GeolocationRequestDTO(double lat, double lng) {
+}
