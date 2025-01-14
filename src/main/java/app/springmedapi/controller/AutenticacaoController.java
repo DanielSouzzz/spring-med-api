b4ef40f32f4faba46e414b221927e6256d505869
Daniel <danielsouzanovo@gmail.com>
2025-01-14 20:43:51 -0300

    implementei a autenticacao.

--- src/main/java/app/springmedapi/controller/AutenticacaoController.java
+++ src/main/java/app/springmedapi/controller/AutenticacaoController.java
@@ -3,6 +3,8 @@ package app.springmedapi.controller;
 import app.springmedapi.entity.usuarioDTO.DadosAutenticacao;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AutenticacaoController {
 
+    private final AuthenticationManager authenticationManager;
+
+    public AutenticacaoController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
     @RequestMapping
     public ResponseEntity autenticar(@RequestBody @Valid DadosAutenticacao dadosAutenticacao) {
+        var token = new UsernamePasswordAuthenticationToken(dadosAutenticacao.login(), dadosAutenticacao.senha());
+        var authenticaon = authenticationManager.authenticate(token);
+
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/app/springmedapi/entity/Usuario.java
+++ src/main/java/app/springmedapi/entity/Usuario.java
@@ -2,6 +2,12 @@ package app.springmedapi.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
@@ -10,11 +16,47 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String login;
     private String senha;
+
+    // TODO : Implementar o relacionamento com a entidade Perfil
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
