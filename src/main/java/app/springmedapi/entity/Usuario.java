824f7fe0f87757c5d9c102ca375152a1d3c21f94
daniel <danielsouzanovo@gmail.com>
2025-01-13 23:11:00 -0300

    criando a nova entidade de usuarios para implementar a JWT.

---
+++ src/main/java/app/springmedapi/entity/Usuario.java
@@ -0,0 +1,20 @@
+package app.springmedapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String senha;
+}
