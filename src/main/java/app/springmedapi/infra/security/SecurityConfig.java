c48df308bfce8e3c444fb183d258ce925c8a934e
Daniel <danielsouzanovo@gmail.com>
2025-01-21 20:38:50 -0300

    adicionando os endpoints ao filtro correto.

--- src/main/java/app/springmedapi/infra/security/SecurityConfig.java
+++ src/main/java/app/springmedapi/infra/security/SecurityConfig.java
@@ -2,24 +2,35 @@ package app.springmedapi.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.PasswordManagementDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeRequests()
+                .requestMatchers(HttpMethod.POST, "/auth").permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
--- src/main/java/app/springmedapi/infra/security/TokenService.java
+++ src/main/java/app/springmedapi/infra/security/TokenService.java
@@ -4,10 +4,12 @@ import app.springmedapi.entity.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Date;
 
 @Service
 public class TokenService {
@@ -18,17 +20,28 @@ public class TokenService {
         try {
             var algorithm = Algorithm.HMAC256(secret);
              return JWT.create()
-                    .withIssuer("API SpringMed")
+                    .withIssuer("SpringMed API")
                      .withSubject(user.getLogin())
                      .withClaim("id", user.getId())
-                     .withExpiresAt(expirationDate())
+                     .withExpiresAt(Date.from(expirationDate()))
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token jwt", exception);
         }
     }
 
-    public Instant expirationDate() {
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("SpringMed API")
+                    .build().verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Erro ao verificar token jwt", exception);
+        }
+    }
+    private Instant expirationDate() {
         return Instant.now().plusSeconds(3600);
     }
 }
