f4f14b3d10a55b234087f99d453c611ed7c3681a
Daniel <danielsouzanovo@gmail.com>
2025-03-21 22:31:12 -0300

    corrige o erro que impossibilidatava a criacao do agendamento.

--- src/main/java/app/springmedapi/service/AgendamentoService.java
+++ src/main/java/app/springmedapi/service/AgendamentoService.java
@@ -1,7 +1,10 @@
 package app.springmedapi.service;
 
+import app.springmedapi.entity.Agendamento;
 import app.springmedapi.entity.AgendamentoDTO.AgendamentoRequestDTO;
 import app.springmedapi.entity.AgendamentoDTO.AgendamentoResponseDTO;
+import app.springmedapi.mapper.AgendamentoMapper;
+import app.springmedapi.repository.AgendamentoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,8 +18,8 @@ public class AgendamentoService {
     }
 
     public AgendamentoResponseDTO agendar(AgendamentoRequestDTO agendamentoRequestDTO) {
-        Agendamento agendamento = agendamentoMapper.toEntity(agendamentoRequestDTO);
-        Agendamento agendamentoSalvo = agendamentoRepository.save(agendamento);
-        return agendamentoMapper.toDTO(agendamentoSalvo);
+        Agendamento agendamentoEntity = agendamentoMapper.toAgendamentoEntity(agendamentoRequestDTO);
+        agendamentoEntity = agendamentoRepository.save(agendamentoEntity);
+        return agendamentoMapper.toAgendamentoDTO(agendamentoEntity);
     }
 }
