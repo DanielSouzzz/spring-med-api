bf4e70da55998f5537a100ded9ea0e318ee66bcd
Daniel <danielsouzanovo@gmail.com>
2025-03-21 21:54:23 -0300

    Adicionando anotacoes de controle de formulario ao dto.

--- src/main/java/app/springmedapi/entity/AgendamentoDTO/AgendamentoResponseDTO.java
+++ src/main/java/app/springmedapi/entity/AgendamentoDTO/AgendamentoResponseDTO.java
@@ -1,10 +1,18 @@
 package app.springmedapi.entity.AgendamentoDTO;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
 public record AgendamentoResponseDTO(
         Long id,
+        @NotNull
+        @Future
         String data,
+        @NotNull
         String medico,
+        @NotNull
         String paciente,
+        @NotNull
         String especialidade
 ) {
 }
