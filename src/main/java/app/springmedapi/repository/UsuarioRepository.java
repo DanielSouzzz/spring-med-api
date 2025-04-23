9723704b814a53b3a01b8e968d6238d8b95dcfff
Daniel <danielsouzanovo@gmail.com>
2025-04-22 22:19:14 -0300

    ajustes no container

--- src/main/java/app/springmedapi/repository/UsuarioRepository.java
+++ src/main/java/app/springmedapi/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package app.springmedapi.repository;
 
 import app.springmedapi.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByLogin(String login);
 }
