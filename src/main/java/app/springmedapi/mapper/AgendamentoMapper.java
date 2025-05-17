1221a6bdf5e05af72df06c0cc716bb99d31ece17
Daniel <danielsouzanovo@gmail.com>
2025-05-17 12:10:04 -0300

    fix(medico): ignora id do medico no dto para tratar no service

--- src/main/java/app/springmedapi/mapper/AgendamentoMapper.java
+++ src/main/java/app/springmedapi/mapper/AgendamentoMapper.java
@@ -4,11 +4,13 @@ import app.springmedapi.entity.Agendamento;
 import app.springmedapi.entity.AgendamentoDTO.AgendamentoRequestDTO;
 import app.springmedapi.entity.AgendamentoDTO.AgendamentoResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
     public interface AgendamentoMapper {
 
     AgendamentoResponseDTO toAgendamentoDTO(Agendamento agendamentoEntity);
 
+    @Mapping(target = "idMedico", ignore = true)
     Agendamento toAgendamentoEntity(AgendamentoRequestDTO agendamentoRequestDTO);
 }
