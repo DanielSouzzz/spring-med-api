caa4df2891976902c6cddf131b64dc392bca97ad
Daniel <danielsouzanovo@gmail.com>
2025-06-03 23:55:45 -0300

    feat: criar query para buscar horarios disponiveis

--- src/main/java/app/springmedapi/repository/AgendamentoRepository.java
+++ src/main/java/app/springmedapi/repository/AgendamentoRepository.java
@@ -2,6 +2,16 @@ package app.springmedapi.repository;
 
 import app.springmedapi.entity.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+    @Query(value = """
+            select a.data from agendamentos a
+            where a.data = :data
+            and a.id_medico = :id_medico
+       """, nativeQuery = true)
+    Integer findRandomDoctor(@Param("data") LocalDateTime data,@Param("id_medico") String id_medico);
 }
