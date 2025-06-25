ce723a7cd8e63796829003fa16774391654fe34f
Daniel <danielsouzanovo@gmail.com>
2025-06-24 21:11:17 -0300

    feat: criar no banco o objeto agenda

--- src/main/java/app/springmedapi/service/AgendaService.java
+++ src/main/java/app/springmedapi/service/AgendaService.java
@@ -1,11 +1,14 @@
 package app.springmedapi.service;
 
+import app.springmedapi.entity.Agenda;
+import app.springmedapi.entity.Agendamento;
 import app.springmedapi.entity.agenda.AgendaRequestDTO;
 import app.springmedapi.entity.agenda.AgendaResponseDTO;
 import app.springmedapi.mapper.AgendaMapper;
 import app.springmedapi.repository.AgendaRepository;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AgendaService {
@@ -17,6 +20,10 @@ public class AgendaService {
         this.agendaMapper = agendaMapper;
     }
 
-    public AgendaResponseDTO liberarHorarios(@Valid AgendaRequestDTO agendaResquestDTO) {
+    @Transactional
+    public AgendaResponseDTO liberarHorarios(@Valid AgendaRequestDTO dto) {
+        Agenda agendaEntity = agendaMapper.toAgendaEntity(dto);
+        agendaEntity = agendaRepository.save(agendaEntity);
+        return agendaMapper.toAgendaDTO(agendaEntity);
     }
 }
