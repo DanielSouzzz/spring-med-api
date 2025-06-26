03ee6aebf8b8fac933fbcd4af95e0563102da544
Daniel <danielsouzanovo@gmail.com>
2025-06-25 22:58:25 -0300

    feat: criar entidade Agenda para agendamentos

--- src/main/java/app/springmedapi/entity/Agenda.java
+++ src/main/java/app/springmedapi/entity/Agenda.java
@@ -1,4 +1,28 @@
 package app.springmedapi.entity;
 
+import app.springmedapi.enums.DayOfWeekEnum;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "available_schedule")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
 public class Agenda {
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer idMedico;
+    private Integer idPaciente;
+    private String especialidade;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "day_of_week", nullable = false)
+    private DayOfWeekEnum dayOfWeek;}
