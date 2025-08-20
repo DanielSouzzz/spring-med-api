8e8e7870f63aa3992c7082ec77a29025d0861a31
Daniel <danielsouzanovo@gmail.com>
2025-08-20 13:31:49 -0300

    feat(service): criar metodo validador de horario de funcionamento.

---
+++ src/main/java/app/springmedapi/service/validacoes/ValidarHorarioDeFuncionamento.java
@@ -0,0 +1,20 @@
+package app.springmedapi.service.validacoes;
+
+import app.springmedapi.entity.AgendamentoDTO.AgendamentoResponseDTO;
+import app.springmedapi.infra.exception.ValidacaoException;
+import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+
+@Service
+public class ValidarHorarioDeFuncionamento {
+    public void validar(AgendamentoResponseDTO dto){
+        var dataAgendamento = dto.data();
+        var isDomingo = dataAgendamento.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var isForaDeFuncionamento = dataAgendamento.getHour() < 7 || dataAgendamento.getHour() > 18;
+
+        if (isDomingo || isForaDeFuncionamento){
+            throw new ValidacaoException("Fora do horário de funcionamento.");
+        }
+    }
+}
