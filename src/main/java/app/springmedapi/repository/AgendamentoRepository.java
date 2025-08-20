b89b9ac7c0863ec663e03287a9bbcfd56350cfb1
Daniel <danielsouzanovo@gmail.com>
2025-08-20 20:25:42 -0300

    feat: adicionar query de verificacao para buscar agendamentos do dia

--- src/main/java/app/springmedapi/repository/AgendamentoRepository.java
+++ src/main/java/app/springmedapi/repository/AgendamentoRepository.java
@@ -1,6 +1,7 @@
 package app.springmedapi.repository;
 
 import app.springmedapi.entity.Agendamento;
+import org.postgresql.core.NativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,11 +9,19 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+    @Query(value = """
+        select 1 from agendamentos a
+        where a.id_paciente = :idPaciente
+        and a.data >= CAST(:data AS DATE)
+        and a.data < CAST(:data AS DATE) + INTERVAL '1 day'
+""", nativeQuery = true)
+    boolean isHasScheduleOnDay(@Param("idPaciente") Long idPaciente,
+                               @Param("data") LocalDateTime data);
 
     @Query(value = """
             select exists (
             SELECT 1 FROM agendamentos
-            WHERE id_medico = :idMedico AND data = :data
+            WHERE idMedico = :idMedico AND data = :data
             )
             """, nativeQuery = true)
     boolean hourAvailable(@Param("idMedico") Long idMedico,
